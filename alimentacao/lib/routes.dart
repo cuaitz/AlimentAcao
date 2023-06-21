317e64e129867f59499386ee42a7507154ec9221
cuaitz <98088769+cuaitz@users.noreply.github.com>
2023-06-21 02:10:05 -0300

    Alterado o nome dos arquivos para "Register"[...]

--- alimentacao/lib/routes.dart
+++ alimentacao/lib/routes.dart
@@ -1,11 +1,11 @@
-import 'package:alimentacao/view/donator_login.dart';
-import 'package:alimentacao/view/receiver_login.dart';
+import 'package:alimentacao/view/donator_register.dart';
+import 'package:alimentacao/view/receiver_register.dart';
 import 'package:flutter/material.dart';
 
 
 class Routes {
-  static const String loginDonatorPage = '/login/donator';
-  static const String receiverDonatorPage = '/login/receiver';
+  static const String loginDonatorPage = '/auth/register/donator';
+  static const String receiverDonatorPage = '/auth/register/receiver';
 
   static Map<String, Widget Function(BuildContext)> routes = {
     loginDonatorPage: (context) => const DonatorLoginScreen(),
