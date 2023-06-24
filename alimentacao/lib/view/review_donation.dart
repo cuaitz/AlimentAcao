372c2247946ffdcce1dfaa19d75a9794c127f4d2
cuaitz <98088769+cuaitz@users.noreply.github.com>
2023-06-24 20:52:37 -0300

    Adicionado a tela de confirmação de doação

---
+++ alimentacao/lib/view/review_donation.dart
@@ -0,0 +1,116 @@
+import 'package:alimentacao/model/donatable_item.dart';
+import 'package:flutter/material.dart';
+import '../components.dart';
+import '../routes.dart';
+
+class ReviewDonationScreen extends StatefulWidget {
+  const ReviewDonationScreen({super.key, required this.donationItems});
+  
+  final Map<int, dynamic> donationItems;
+
+  @override
+  State<ReviewDonationScreen> createState() => _ReviewDonationScreenState();
+}
+
+class _ReviewDonationScreenState extends State<ReviewDonationScreen> {
+  @override
+  Widget build(BuildContext context) {
+    final List<int> itemsID = widget.donationItems.keys.toList();
+    final List<dynamic> itemsData = widget.donationItems.values.toList(); 
+    
+    return Scaffold(
+      appBar: Components.appBar("Doação", backgroundColor: Components.colorLightPurple),
+      bottomNavigationBar: Container(
+        color: Components.colorPurple,
+        height: 200,
+        child: Padding(
+          padding: const EdgeInsets.symmetric(horizontal: 16, vertical: 10),
+          child: Center(
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.stretch,
+              mainAxisAlignment: MainAxisAlignment.spaceAround,
+              children: [
+                Components.quickText("Ponto de coleta:", fontSize: 24, fontWeight: FontWeight.w700, color: Colors.white, textAlign: TextAlign.left),
+                Components.uiButton("Selecionar", Colors.white, () { Navigator.pushNamed(context, Routes.donationMapPage);}, textColor: Components.colorPurple),
+                Components.uiButton("Finalizar", Colors.white, () {}, textColor: Components.colorPurple),
+              ],
+            )
+          ),
+        ),
+      ),
+      body: SingleChildScrollView(
+        child: SizedBox(
+          height: MediaQuery.of(context).size.height - 200,
+          child: Center(
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: [
+                Container(
+                  color: Components.colorPurple,
+                  height: 120,
+                  width: double.infinity,
+                  child: Center(
+                    child: Components.h1("Revise sua doação", color: Colors.white, textAlign: TextAlign.center)
+                  )
+                ),
+                Expanded(
+                  child: Container(
+                    width: 400,
+                    color: Components.colorLightPurple,
+                    child: Padding(
+                      padding: const EdgeInsets.symmetric(horizontal: 16, vertical: 11),
+                      child: ListView.builder(
+                        itemCount: itemsID.length,
+                        itemBuilder: (context, index) {
+                          return listItem(
+                            DonatableItem.get(itemsID[index]),
+                            itemsData[index]['amount'],
+                            itemsData[index]['unit']
+                          );
+                        }
+                      )
+                    ),
+                  ),
+                ),
+              ],
+            )
+          ),
+        )
+      )
+    );
+  }
+
+
+  Padding listItem(DonatableItem item, int amount, String unit) {
+    return Padding(
+      padding: const EdgeInsets.symmetric(vertical: 5),
+      child: Container(
+        decoration: BoxDecoration(
+          color: Colors.white,
+          borderRadius: BorderRadius.circular(25)
+        ),
+        height: 100,
+        child: Padding(
+          padding: const EdgeInsets.all(10),
+          child: Row(
+            children: [
+              Image(
+                image: item.image,
+                width: 75, 
+                height: 75,
+              ),
+              Column(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Components.quickText(item.name, fontSize: 24, fontWeight: FontWeight.w700, color: Components.colorPurple),
+                  Components.quickText("Quantidade: $amount $unit(s)", fontSize: 18, fontWeight: FontWeight.w700, color: Components.colorPurple)
+                ],
+              )
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
