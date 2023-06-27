9a7d6a47f9d0c5cabe3f9eaa588c8b3f536369ec
cuaitz <98088769+cuaitz@users.noreply.github.com>
2023-06-27 11:22:43 -0300

    Adicionado a validação de dados na tela de registro de doador

--- alimentacao/lib/view/donator_register.dart
+++ alimentacao/lib/view/donator_register.dart
@@ -1,60 +1,110 @@
+import 'package:alimentacao/model/donator.dart';
 import 'package:flutter/material.dart';
 import '../components.dart';
 import '../routes.dart';
 
-
-class DonatorRegisterScreen extends StatelessWidget {
+class DonatorRegisterScreen extends StatefulWidget {
   const DonatorRegisterScreen({super.key});
+
+  @override
+  State<DonatorRegisterScreen> createState() => _DonatorRegisterScreenState();
+}
+
+class _DonatorRegisterScreenState extends State<DonatorRegisterScreen> {
+  final GlobalKey<FormState> _formKey = GlobalKey<FormState>();
+
+  final TextEditingController _businessNameController = TextEditingController();
+  final TextEditingController _fantasyNameController = TextEditingController();
+  final TextEditingController _addressNameController = TextEditingController();
+  final TextEditingController _cnpjController = TextEditingController();
+  final TextEditingController _emailController = TextEditingController();
+  final TextEditingController _passwordController = TextEditingController();
+  final TextEditingController _passwordConfirmationController = TextEditingController();
+
+  void _submitForm() {
+    DonatorUser(
+      _businessNameController.text,
+      _fantasyNameController.text,
+      _addressNameController.text,
+      _cnpjController.text,
+      _emailController.text,
+      _passwordController.text
+    );
+  }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: Components.appBar("Registro", backgroundColor: Components.colorPurple),
       body: SingleChildScrollView(
-        child: Center(
-          child: Padding(
-            padding: const EdgeInsets.symmetric(horizontal: 16),
-            child: Column(
-              crossAxisAlignment: CrossAxisAlignment.center,
-              children: [
-                Padding(
-                  padding: const EdgeInsets.all(12),
-                  child: Container(
-                    decoration: BoxDecoration(
-                      border: Border.all(
-                        color: Components.colorPurple,
-                        width: 2
-                      ),
-                      borderRadius: BorderRadius.circular(100)
-                    ),
-                    child: const Icon(
-                      Icons.person,
-                      size: 100,
+        child: Padding(
+          padding: const EdgeInsets.symmetric(horizontal: 16),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.center,
+            children: [
+              Padding(
+                padding: const EdgeInsets.all(12),
+                child: Container(
+                  decoration: BoxDecoration(
+                    border: Border.all(
                       color: Components.colorPurple,
+                      width: 2
                     ),
+                    borderRadius: BorderRadius.circular(100)
+                  ),
+                  child: const Icon(
+                    Icons.person,
+                    size: 100,
+                    color: Components.colorPurple,
                   ),
                 ),
-                SizedBox(
-                  height: 520,
+              ),
+              SizedBox(
+                child: Form(
+                  key: _formKey,
                   child: Column(
                     mainAxisAlignment: MainAxisAlignment.spaceBetween,
                     children: [
-                      Components.uiTextField("Razão social", Components.colorPurple),
-                      Components.uiTextField("Nome fantasia", Components.colorPurple),
-                      Components.uiTextField("Endereço", Components.colorPurple),
-                      Components.uiTextField("CNPJ", Components.colorPurple),
-                      Components.uiTextField("Email", Components.colorPurple),
-                      Components.uiTextField("Senha", Components.colorPurple, obscureText: true),
-                      Components.uiTextField("Confirme sua senha", Components.colorPurple, obscureText: true),
-                      Components.uiButton("Cadastrar", Components.colorPurple, () { Navigator.pushNamed(context, Routes.loginPage); })
+                      paddedTextField("Razão social", Components.colorPurple, controller: _businessNameController),
+                      paddedTextField("Nome fantasia", Components.colorPurple, controller: _fantasyNameController),
+                      paddedTextField("Endereço", Components.colorPurple, controller: _addressNameController),
+                      paddedTextField("CNPJ", Components.colorPurple, controller: _cnpjController),
+                      paddedTextField("Email", Components.colorPurple, controller: _emailController),
+                      paddedTextField("Senha", Components.colorPurple, obscureText: true, controller: _passwordController),
+                      paddedTextField("Confirme sua senha", Components.colorPurple, obscureText: true, controller: _passwordConfirmationController,
+                      validator: (str) {
+                        if (str!.isEmpty) { return 'Preencha este campo.'; }
+                        if (str != _passwordController.text) { return 'As senhas informadas não coincidem.'; }
+                        return null;
+                      },),
+                      Padding(
+                        padding: const EdgeInsets.only(bottom: 10, top: 2.5),
+                        child: Components.uiButton(
+                          "Cadastrar",
+                          Components.colorPurple,
+                          () {
+                            if (_formKey.currentState!.validate()) {
+                              _submitForm();
+                              Navigator.pushNamed(context, Routes.loginPage);
+                            }
+                          }
+                        ),
+                      )
                     ],
                   ),
-                )
-              ]
-            ),
+                ),
+              )
+            ]
           ),
         ),
       )
+    );
+  }
+
+  Widget paddedTextField(String labelText, Color themeColor, {String? Function(String?)? validator, TextEditingController? controller, Color? backgroundColor, String? hintText, bool obscureText = false}) {
+    return Padding(
+      padding: const EdgeInsets.symmetric(vertical: 2.5),
+      child: Components.uiTextField(labelText, themeColor, validator: validator, controller: controller, backgroundColor: backgroundColor, hintText: hintText, obscureText: obscureText)
     );
   }
 }
